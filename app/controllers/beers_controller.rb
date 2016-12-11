d420ec2b382819cc54ea8766efaa679efe2da374
Linda-Diamond-Robbins <lindadiamondrobbins@gmail.com>
2016-12-11 11:28:41 -0600

    Make an env

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,12 +1,58 @@
 class BeersController < ApplicationController
+  def index
+    # @beers = Beer.all
+    @beers = Unirest.get(
+      "http://localhost:3000/api/v1/beers",
+      headers: { 'Authorization' => "Token token=#{ENV['API_KEY']}", 'X-User-Email' => ENV['API_EMail']}
+      ).body
+    render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    @beer = Unirest.post(
+      "http://localhost:3000/api/v1/beers",
+      parameters: {
+        name: params[:name],
+        style: params[:style],
+        alcohol: params[:alcohol],
+        ibu: params[:ibu]
+      }
+    ).body
+    redirect_to "/beers/#{@beer["id"]}"
+  end
+
+
   def show
     # @beer = Beer.find_by(id: params[:id])
-    @beer = Unirest.get("http://localhost:3000/beers/#{params[:id]}").body
+    @beer = Unirest.get("http://localhost:3000/api/v1/beers/#{params[:id]}").body
     render 'show.html.erb'
   end
 
-  def index
-    @beers = Unirest.get("http://localhost:3000/beers").body
-    render 'index.html.erb'
+  def edit
+    @beer = Unirest.get("http://localhost:3000/api/v1/beers/#{params[:id]}").body
+    render 'edit.html.erb'
+  end
+
+  def update
+    @beer = Unirest.patch(
+      "http://localhost:3000/api/v1/beers/#{params[:id]}",
+      parameters: {
+        name: params[:form_name],
+        style: params[:style],
+        alcohol: params[:alcohol],
+        ibu: params[:ibu]
+      }
+    ).body
+    redirect_to "/beers/#{@beer['id']}"
+  end
+
+  def destroy
+    message = Unirest.delete("http://localhost:3000/api/v1/beers/#{params[:id]}").body
+    redirect_to "/beers"
   end
 end
+Contact GitHub API Training Shop Blog About
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/beers/:id' => 'beers#show'
   get '/beers' => 'beers#index'
+  get '/beers/new' => 'beers#new'
+  post '/beers' => 'beers#create'
+  get '/beers/:id' => 'beers#show'
+  get '/beers/:id/edit' =>'beers#edit'
+  patch '/beers/:id' => 'beers#update'
+  delete '/beers/:id' => 'beers#destroy'
+
 
 end
